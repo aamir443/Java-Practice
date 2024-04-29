5ecbbcfcb716b309c02c546029aca03f134308b5
Mohammad Aamir <121339457+aamir443@users.noreply.github.com>
2024-04-29 20:38:10 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class A 
+{
+    public static void main(String[] args)
+    {
+        int a;
+        Scanner obj=new Scanner(System.in);
+        System.out.print("enter value");
+        a=obj.nextInt();
+        System.out.println("a");
+     }
+
+}
